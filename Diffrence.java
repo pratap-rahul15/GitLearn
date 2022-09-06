0f689bd0183b9f65f3972066383c9d160e04a264
Rahul Singh <rahul660singh@gmail.com>
2022-09-06 12:35:07 +0530

    initial 1 commit

--- Diffrence.java
+++ Diffrence.java
@@ -2,6 +2,6 @@ public class Diffrence {
     public static void main(String[] args) {
         int a = 78, b  = 23;
         int res = 0;
-        System.out.println(res = a-b);
+        System.out.println("The diffrence between two numbers is:" +(res = a-b));
     }
 }
