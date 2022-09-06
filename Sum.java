62db44ce5fb6862af8480c679a96c450356d8450
Rahul Singh <rahul660singh@gmail.com>
2022-09-06 12:20:22 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum {
+    public static void main(String[] args) {
+        // Addition of two numbers
+        int a = 23;
+        int b = 89;
+        int res = 0;
+
+        System.out.println(res = a + b);
+    }
+}
