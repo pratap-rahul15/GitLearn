f1a98814e63d5a7446f164577fca9a79d03cbe01
Rahul Singh <rahul660singh@gmail.com>
2022-09-06 13:07:32 +0530

    added in rahul/mutiply branch

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 78, b  = 23;
+        int res = 0;
+        System.out.println("The diffrence between two numbers is:" +(res = a*b));
+    }
+}
