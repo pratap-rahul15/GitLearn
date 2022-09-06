8a6d95321307993ebc90a5cb55a7e33adaedfbd9
Rahul Singh <rahul660singh@gmail.com>
2022-09-06 13:59:02 +0530

    added some comments

--- Diffrence.java
+++ Diffrence.java
@@ -2,6 +2,7 @@ public class Diffrence {
     public static void main(String[] args) {
         int a = 78, b  = 23;
         int res = 0;
+        // Finding the minus, diffrence between two given soft-code values.
         System.out.println("The diffrence between two numbers is:" +(res = a-b));
     }
 }
